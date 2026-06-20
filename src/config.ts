54c5a7b379263ea4c7533a229fb8a79dabe4e02b
Nikketa <2700616389@qq.com>
2026-06-21 02:03:01 +0800

    13

--- src/config.ts
+++ src/config.ts
@@ -26,7 +26,7 @@ const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UT
 export const siteConfig: SiteConfig = {
 	title: "Geinka's Blog",
 	subtitle: "Somethine about me...",
-	siteURL: "https://kazepu.github.io/", // 请替换为你的站点URL，以斜杠结尾
+	siteURL: "https://kazepu.pages.dev/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2023-12-24", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
